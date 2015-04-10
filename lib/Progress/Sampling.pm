355bc66edf105a2bbab604bb82493f3419f5fc37
Kuniyoshi Kouji <kuniyoshi@seesaa.co.jp>
2015-04-10 19:13:24 +0900

    enable variable format

--- lib/Progress/Sampling.pm
+++ lib/Progress/Sampling.pm
@@ -1,87 +1,116 @@
 use strict;
 use warnings;
 package Progress::Sampling;
+use fields qw( max interval format _format _format_fields _started_at _count _elapsed );
 use Time::HiRes qw( gettimeofday tv_interval );
 use Readonly;
 
 Readonly my %DEFAULT => (
     interval => 10,
+    format   => "__PERCENTAGE__[%] = __COUNT__/__MAX__,\t__SPEED__,\t__REMAIN__[s] remain",
 );
-
+Readonly my %FORMAT => (
+    __PERCENTAGE__ => q{%d},
+    __COUNT__      => q{%d},
+    __MAX__        => q{%d},
+    __SPEED__      => q{%.4f},
+    __REMAIN__     => q{%d},
+);
 
 sub new {
     my $class = shift;
     my %param = @_;
 
-    my $max      = delete $param{max};
-    my $interval = delete $param{interval};
+    $param{ $_ } = $DEFAULT{ $_ }
+        for grep { !exists $param{ $_ } } keys %DEFAULT;
 
-    my $now = [ gettimeofday ];
+    my $self = fields::new( $class );
 
-    my $self = bless {
-        _max        => $max,
-        _started_at => $now,
-        _interval   => $interval,
-    }, $class;
+    @{ $self }{ keys %param } = values %param;
 
-    return $self;
+    return $self->init;
+}
+
+sub gen_format {
+    my $self       = shift;
+    my $format_str = shift;
+    my @fields;
+
+    $format_str =~ s{[%]}{%%}g;
+    $format_str =~ s{(__[A-Z]+__)}{
+        push @fields, $1;
+        $FORMAT{ $1 };
+    }egmsx;
+
+    return ( $format_str, \@fields );
 }
 
 sub init {
     my $self = shift;
     $self->{_started_at} = [ gettimeofday ];
-    return;
+    @{ $self }{ qw( _format _format_fields ) } = $self->gen_format( $self->{format} );
+    return $self;
 }
-
-sub started_at { shift->{_started_at} }
-
-sub current { shift->{_current} }
-
-sub interval { shift->{_interval} || $DEFAULT{interval} }
 
 sub worked {
     my $self = shift;
 
-    $self->{_current}++;
-    my $need_to_generate = ( $self->current % $self->interval ) == 0;
+    $self->{_count}++;
+
+    my $need_to_generate = ( $self->{_count} % $self->{interval} ) == 0;
 
     return
         unless $need_to_generate;
 
-    $self->{_elapsed} = tv_interval( $self->started_at );
+    $self->{_elapsed} = tv_interval( $self->{_started_at} );
 
     return $self->format;
 }
 
-sub max { shift->{_max} }
-
-sub progress { shift->{_progress} }
-
-sub elapsed { shift->{_elapsed} }
+sub percentage {
+    my $self = shift;
+    return ( ( $self->{_count} / $self->{max} ) * 100 );
+}
 
 sub speed {
     my $self = shift;
     return
-        unless $self->elapsed;
-    return $self->current / $self->elapsed;
-}
-
-sub percentage {
-    my $self = shift;
-    return ( ( $self->current / $self->max ) * 100 );
+        unless $self->{_elapsed};
+    return $self->{_count} / $self->{_elapsed};
 }
 
 sub remain {
     my $self = shift;
-    my $remain_count = $self->max - $self->current;
+    my $remain_count = $self->{max} - $self->{_count};
     my $speed        = $self->speed
         or return;
     return $remain_count / $speed;
 }
 
+sub _get_field_value {
+    my $self = shift;
+    my $name = shift;
+    my $original = $name;
+    $name =~ s{\A __ }{}msx;
+    $name =~ s{ __ \z}{}msx;
+    $name = lc $name;
+    return $self->{ $name }
+        if exists $self->{ $name };
+    return $self->$name
+        if $self->can( $name );
+    $name = "_$name";
+    return $self->{ $name }
+        if exists $self->{ $name };
+    return $self->$name
+        if $self->can( $name );
+    die "Could not get value $original -> $name";
+}
+
 sub format {
     my $self = shift;
-    return sprintf "%d[%%] = %d/%d,\t%.4f[w/s],\t%d[s] remain", $self->percentage, $self->current, $self->max, $self->speed, $self->remain;
+    return sprintf $self->{_format},
+        map { $self->_get_field_value( $_ ) }
+        @{ $self->{_format_fields} };
 }
 
 1;
